96ef793b1bd42c9ddaafa6232c483765b0303210
Audrey Evans <aevans@1005d-9.eecs.ku.edu>
2016-02-15 15:14:15 -0600

    add e1.js and logic, add button to excersize

---
+++ e1.js
@@ -0,0 +1,28 @@
+
+//Audrey Evans
+// 2/15/16
+// my first javascript i guess
+
+function myFunc(p1,p2)
+{
+	alert(p1);
+	if(p1 == p2)
+	{
+		if(p1<=8)
+			{alert("password not long enough");}
+		else 
+		{
+			alert("passwords match!");
+		}
+				
+		
+	}
+	else 
+	{
+		alert("passwords do not match");
+	}
+	
+		
+	
+	alert(p1);
+}
